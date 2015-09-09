dc7ebb4392f011fd82a4b653fd5b7cce954688ba
Abhas Arya <abhasarya@Abhass-MacBook-Pro.local>
2015-09-09 14:03:14 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fitness44::Application.config.secret_key_base = 'd5bf54f7b9d2110831666bf957c824ecff08406c4497eb12b3ac19512018df59c17024210166d58e8dd8de45e5e3ef91bb72bf74bd0e19fb256213957e475f08'
