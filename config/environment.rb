096a07c4924ec3b9c0646047912a077422601725
Abhas Arya <abhasarya@Abhass-MacBook-Pro.local>
2016-01-12 19:45:05 -0500

    switched email from ActionMailer to rely on SendGrid API

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Fitness44::Application.initialize!
+
+
+# should store these credentials in environment variables on the server
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
